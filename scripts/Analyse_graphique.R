cad322d11a6b100537573bf72b5fb8d697a30871
Lucas BERTHOU <lucas.berthou3@gmail.com>
2025-02-18 00:22:15 +0100

    Analyse graphique (décomposition saisonnière, ACF, PACF)

--- scripts/Analyse_graphique.R
+++ scripts/Analyse_graphique.R
@@ -3,8 +3,18 @@ Excel_trafic_routier <- read_excel("data/raw/Excel - trafic routier.xlsx", col_n
 
 View(Excel_trafic_routier)
 
+#création de la série temporelle
 trafic_routier <- ts(Excel_trafic_routier[,2], start=c(2001,01),end=c(2024,09),frequency=12)
 
-
+# graphique chronologique de la série
 plot(trafic_routier,main="Trafic routier mensuel sur routes nationales",xlab="t",ylab="milliards de voitures-kilomètres")
 
+# décomposition de la série temporelle pour isoler sa tendance, saisonnalité et aléatoire
+decomp.x=decompose(trafic_routier,type="multiplicative")
+decomp.x$figure
+plot(decomp.x)
+
+# corrélogrammes
+layout(matrix(1:2,1,2))
+acf(c(trafic_routier),lag.max= NULL, type = c("correlation"),plot = TRUE, col="cyan", main=element_blank())
+acf(c(trafic_routier),lag.max= NULL, type = c("partial"),plot = TRUE, col="red",main=element_blank())
