66785f9fe93481cce8af0088bd40c48dd73fc220
Lucas BERTHOU <lucas.berthou3@gmail.com>
2025-02-17 20:55:24 +0100

    je suis un gland

--- Analyse_graphique.R
+++ Analyse_graphique.R
@@ -1,7 +1,10 @@
 library(readxl)
 Excel_trafic_routier <- read_excel("data/raw/Excel - trafic routier.xlsx", col_names = TRUE, col_types = NULL) 
+
+View(Excel_trafic_routier)
+
 trafic_routier <- ts(Excel_trafic_routier[,2], start=c(2001,01),end=c(2024,09),frequency=12)
 
 
-plot(trafic_routier,main="Trafic routier mensuel sur routes nationales",xlab="date",ylab="milliards de voitures-kilomètres")
+plot(trafic_routier,main="Trafic routier mensuel sur routes nationales",xlab="t",ylab="milliards de voitures-kilomètres")
 
